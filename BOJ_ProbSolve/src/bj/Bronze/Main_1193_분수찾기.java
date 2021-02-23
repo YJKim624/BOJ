3fb44399a8642e6643c4e15f69c030cee275e12b
YJKIM <rladbswo624@gmail.com>
2021-02-24 02:13:16 +0900

    0204_commit

--- BOJ_ProbSolve/src/bj/Bronze/Main_1193_분수찾기.java
+++ BOJ_ProbSolve/src/bj/Bronze/Main_1193_분수찾기.java
@@ -8,10 +8,10 @@ import java.io.InputStreamReader;
  * @since 2021. 2. 18.
  * @author yjkim
  * @see https://www.acmicpc.net/problem/1193
- * @mem 
- * @time 
+ * @mem 16688
+ * @time 88
  * @caution 
- * 
+ * i/j
  */
 
 public class Main_1193_분수찾기 {
@@ -19,47 +19,28 @@ public class Main_1193_분수찾기 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	static int x; // x번째 분수 찾기
-	static int[][] arr; // 분수 넣을 배열
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		x = Integer.parseInt(br.readLine());
 
-		arr = new int[x + 1][2];
-
-		int idx = 1;
 		int cnt = 1;
+		int sum = 0;
 
-		outer: while (idx <= x) {
-			int start = cnt;
-			int j = 1;
-			if (cnt % 2 != 0) {
-				while (j <= cnt) {
-					arr[idx][0] = start;
-					arr[idx][1] = j;
-					start--;
-					j++;
-					idx++;
-					if (idx > x) {
-						break outer;
-					}
-				}
-			} else if (cnt % 2 == 0) {
-				while (j <= cnt) {
-					arr[idx][0] = j;
-					arr[idx][1] = start;
-					start--;
-					j++;
-					idx++;
-					if (idx > x) {
-						break outer;
-					}
-				}
-			}
+		while (sum < x) {
+			sum += cnt;
 			cnt++;
 		}
 
-		System.out.println(arr[x][0] + "/" + arr[x][1]);
+		if (cnt % 2 != 0) {
+			int i = cnt - (sum - x) - 1;
+			int j = 1 + (sum - x);
+			System.out.println(i + "/" + j);
+		} else {
+			int i = 1 + (sum - x);
+			int j = cnt - (sum - x) - 1;
+			System.out.println(i + "/" + j);
+		}
 
 	}
 
--- BOJ_ProbSolve/src/bj/Bronze/Main_2999_비밀이메일.java
+++ BOJ_ProbSolve/src/bj/Bronze/Main_2999_비밀이메일.java
@@ -7,13 +7,13 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 /*
- * @since 2021. 2. 20.
+ * @since 2021. 2. 24.
  * @author yjkim
  * @see https://www.acmicpc.net/problem/2999
- * @mem 
- * @time 
+ * @mem 14292
+ * @time 124
  * @caution 
- * 
+ * 1. 입력할 땐 세로로, 출력할 땐 가로로(헷갈림 주의)
  */
 
 public class Main_2999_비밀이메일 {
@@ -26,49 +26,36 @@ public class Main_2999_비밀이메일 {
 	static char[][] arr; // R*C=N인 배열 생성
 	static int row, col; // 최종 배열 row, col
 
-	static Stack<Integer> R = new Stack<>();
-	static Stack<Integer> C = new Stack<>();
+	static int R;
+	static int C;
 
 	public static void main(String[] args) throws IOException {
 
-		s = br.readLine();	// 메세지 문자열 입력
-		int len = s.length();	// 문자열 길이
+		s = br.readLine(); // 메세지 문자열 입력
+		int len = s.length(); // 문자열 길이
 
+		row = 0;
+		col = 0;
 		for (int r = 1; r <= len; r++) {
 			for (int c = 1; c <= len; c++) {
-				if (r <= c && r * c == len) {	// r*c가 문자열 길이인 것들 중 r<=c 인 두 숫자 r, c 스택에 push
-					R.push(r);
-					C.push(c);
+				if (r <= c && r * c == len) { // r*c가 문자열 길이인 것들 중 r<=c 인 두 숫자 r, c 스택에 push
+					row = r;
+					col = c;
 				}
 			}
 		}
 
-		row = R.pop();	// row는 r 중 가장 큰 값
-		col = C.pop();	// col은 row에 대응하는 값
-
-//		// 확인용
-//		System.out.println(row);
-//		System.out.println(col);
-
-		arr = new char[row][col];	// row*col 크기의 배열 생성
-		int cnt = 0;	// 배열 char 입력용
-		for (int r = 0; r < row; r++) {
-			for (int c = 0; c < col; c++) {
-				arr[r][c] = s.charAt(cnt);	// 배열에 char 입력(cnt=0번째부터~)
+		arr = new char[row][col]; // row*col 크기의 배열 생성
+		int cnt = 0; // 배열 char 입력용
+		for (int i = 0; i < col; i++) {
+			for (int j = 0; j < row; j++) {
+				arr[j][i] = s.charAt(cnt); // 배열에 char 입력(cnt=0번째부터~)
 				cnt++;
 			}
 		}
 
-//		// 입력 확인용
-//		for (int r = 0; r < row; r++) {
-//			for (int c = 0; c < col; c++) {
-//				System.out.print(arr[r][c] + " ");
-//			}
-//			System.out.println();
-//		}
-
-		for (int c = 0; c < col; c++) {
-			for (int r = 0; r < row; r++) {
+		for (int r = 0; r < row; r++) {
+			for (int c = 0; c < col; c++) {
 				sb.append(arr[r][c]);
 			}
 		}
